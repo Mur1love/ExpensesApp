cc15581a1a955f932879ef5759a43a9cbb565bf9
Mur1love <murilormleal@outlook.com>
2024-01-03 17:48:36 -0300

    refatorando mediaquery

--- lib/main.dart
+++ lib/main.dart
@@ -90,8 +90,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    final mediaQuery = MediaQuery.of(context);
+
     bool isLandScape =
-        MediaQuery.of(context).orientation == Orientation.landscape;
+        mediaQuery.orientation == Orientation.landscape;
 
     final appBar = AppBar(
       title: const Text('Despesas Pessoais'),
@@ -112,9 +115,9 @@ class _MyHomePageState extends State<MyHomePage> {
         
       ],
     );
-    final availableHeight = MediaQuery.of(context).size.height -
+    final availableHeight = mediaQuery.size.height -
         appBar.preferredSize.height -
-        MediaQuery.of(context).padding.top;
+        mediaQuery.padding.top;
 
     return Scaffold(
       appBar: appBar,
